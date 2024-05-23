4db01069d4375816d56fff66b35cfeaf9fd81a7f
henryjw <3443648+henryjw@users.noreply.github.com>
2024-05-22 22:46:55 -0400

    add expiration functionality

--- src/pages/ViewSnippetPage.tsx
+++ src/pages/ViewSnippetPage.tsx
@@ -3,7 +3,7 @@ import {useParams} from "react-router-dom";
 import {Alert, Flex, Input, Label} from "@aws-amplify/ui-react";
 import {generateClient} from "aws-amplify/api";
 import type {Schema} from "../../amplify/data/resource.ts";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const client = generateClient<Schema>();
 
@@ -13,22 +13,48 @@ export default function ViewSnippetPage() {
     const [err, setError] = useState<Error | null>(null);
     const [message, setMessage] = useState<string | null>(null);
 
-    client.models.Snippet.get({
-        id: snippetId as string
-    }).then((snippet) => {
-        console.log('Snippet:', snippet)
-        if (!snippet.data) {
-            setMessage('Snippet not found. It may have expired or been deleted.');
-            setSnippetContent('');
-            return;
-        }
+    useEffect(() => {
+        client.models.Snippet.get({
+            id: snippetId as string
+        }).then(async (snippet) => {
+            console.log('Snippet:', snippet)
+            if (!snippet.data) {
+                setMessage('Snippet not found. It may have expired or been deleted.');
+                setSnippetContent('');
+                return;
+            }
 
-        setSnippetContent(snippet.data?.content || '');
-    }).catch((err) => {
-        console.error('Error:', err)
-        setError(err.message);
-        setSnippetContent('');
-    });
+            setSnippetContent(snippet.data?.content || '');
+
+            const data = snippet.data;
+            const isExpired = data.burnOnRead || data.expiration && data.expiration < Date.now();
+
+            if (data.burnOnRead) {
+                setMessage('This snippet has been marked for deletion after reading.');
+            }
+
+            if (data.expiration) {
+                const expirationDate = new Date(data.expiration);
+                setMessage(`This snippet will expire on ${expirationDate.toLocaleString()}`);
+            }
+
+            if (!isExpired) {
+                return
+            }
+
+            try {
+                await client.models.Snippet.delete({
+                    id: data.id,
+                });
+            } catch (err) {
+                console.error('Error updating Snippet:', err)
+            }
+        }).catch((err) => {
+            console.error('Error:', err)
+            setError(err.message);
+            setSnippetContent('');
+        });
+    }, [snippetId]);
 
     return (
         // FIXME: there's a lot of duplication here that's common with CreateNewSnippetPage.tsx. Consider refactoring.
@@ -48,18 +74,18 @@ export default function ViewSnippetPage() {
             <Flex direction={{base: 'column', large: 'row'}} justifyContent="center">
                 {/*TODO: add a button / icon to copy contents to clipboard*/}
                 <form style={{width: "100%", maxWidth: "1500px"}}>
-                <Label htmlFor="view-snippet" fontWeight="semibold">Snippet Contents</Label>
-                <Input
-                    id="view-snippet"
-                    as="textarea"
-                    backgroundColor="background.disabled"
-                    height="20rem"
-                    boxShadow="medium"
-                    readOnly={true}
-                    aria-multiline={true}
-                    value={snippetContent}
-                    style={{whiteSpace: 'pre'}} // Preserve line breaks
-                /></form>
+                    <Label htmlFor="view-snippet" fontWeight="semibold">Snippet Contents</Label>
+                    <Input
+                        id="view-snippet"
+                        as="textarea"
+                        backgroundColor="background.disabled"
+                        height="20rem"
+                        boxShadow="medium"
+                        readOnly={true}
+                        aria-multiline={true}
+                        value={snippetContent}
+                        style={{whiteSpace: 'pre'}} // Preserve line breaks
+                    /></form>
             </Flex>
         </Layout>
     )
